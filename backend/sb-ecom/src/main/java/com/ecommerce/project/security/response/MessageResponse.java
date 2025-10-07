01b4b57cdcf171ce2331bdd2c1615a809c0e8d48
Vaibhav Patil <vaibhavpatil5027@gmail.com>
2025-10-07 20:33:05 +0530

    message response added

---
+++ backend/sb-ecom/src/main/java/com/ecommerce/project/security/response/MessageResponse.java
@@ -0,0 +1,15 @@
+package com.ecommerce.project.security.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class MessageResponse {
+    String msg;
+
+    public MessageResponse(String msg) {
+        this.msg = msg;
+    }
+
+}
