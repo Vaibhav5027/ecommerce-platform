39bd415f950fff049315f05ab93d13e652b09c46
Vaibhav Patil <vaibhavpatil5027@gmail.com>
2025-10-07 20:34:17 +0530

    userinfo response

---
+++ backend/sb-ecom/src/main/java/com/ecommerce/project/security/jwt/UserInfoResponse.java
@@ -0,0 +1,62 @@
+package com.ecommerce.project.security.jwt;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import java.util.List;
+
+public class UserInfoResponse {
+    private Long userId;
+    private String jwtToken;
+    private String username;
+    @JsonIgnore
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    private List<String> roles;
+
+    public UserInfoResponse(Long userId, String jwtToken, String username, List<String> roles) {
+        this.userId = userId;
+        this.jwtToken = jwtToken;
+        this.username = username;
+        this.roles = roles;
+    }
+
+    public String getJwtToken() {
+        return jwtToken;
+    }
+
+    public void setJwtToken(String jwtToken) {
+        this.jwtToken = jwtToken;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<String> roles) {
+        this.roles = roles;
+    }
+}
