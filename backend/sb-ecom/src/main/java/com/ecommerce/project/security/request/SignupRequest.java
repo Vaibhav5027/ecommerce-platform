4705df1ebb9a682e6ef5f76617e6c8b2620d3667
Vaibhav Patil <vaibhavpatil5027@gmail.com>
2025-10-07 20:33:59 +0530

    signup request added

---
+++ backend/sb-ecom/src/main/java/com/ecommerce/project/security/request/SignupRequest.java
@@ -0,0 +1,27 @@
+package com.ecommerce.project.security.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.util.Set;
+
+@Data
+public class SignupRequest {
+
+    @NotBlank
+    @Size(min = 3, max = 20, message = "username should not less than 3 characters")
+    private String username;
+
+    @NotBlank
+    @Size(max = 50, message = "please enter valid email")
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 6)
+    private String password;
+
+    private Set<String> roles;
+}
