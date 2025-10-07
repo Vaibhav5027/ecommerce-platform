c964aef061ebf7346091c36f179008b990dd0be3
Vaibhav Patil <vaibhavpatil5027@gmail.com>
2025-10-07 20:34:38 +0530

    user repository  and roles class

--- backend/sb-ecom/src/main/java/com/ecommerce/project/repository/UserRepository.java
+++ backend/sb-ecom/src/main/java/com/ecommerce/project/repository/UserRepository.java
@@ -1,11 +1,16 @@
 package com.ecommerce.project.repository;
 
 import com.ecommerce.project.model.User;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-  Optional<User> findByUsername(String username);
+    Optional<User> findByUsername(String username);
+
+    boolean existByUsername(String username);
+
+    boolean exitsByEmail(String email);
 }
